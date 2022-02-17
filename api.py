94574c012821fb8bd40fcc87ec87776743c07e51
ahutchful <74930349+ahutchful@users.noreply.github.com>
2022-02-16 22:45:36 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,27 +1,25 @@
-
-
 from fastapi import FastAPI
-from pydantic import BaseModel
-
+import pandas as pd
+import numpy as np
+from tensorflow.keras.models import Sequential, load_model
 
 app = FastAPI()
+model = load_model('prediction')
 
-class inputs(BaseModel):
-    CNV: int
-    RPPA: int
-    miRNA: int 
-    
-##insert loading trained model
-
-##loadedmodel = 
+def match(seq1, seq2):
+    """Finds the index locations of seq1 in seq2"""
+    return [ np.nonzero(seq2==x)[0][0] for x in seq1  if x in seq2 ]
 
 
-def survivability(data:inputs):
-    recieved = data.dict()
-    CNV = recieved['CNV']
-    RPPA = recieved['RPPA']
-    miRNA = recieved['miRNA']
-    Result = loadedmodel.predict([[CNV, RPPA, miRNA]]).tolist()[0]
-    
-    return {'Prediction': Result}
+@app.post('/prediction' )
+def get_potability(dataframe: df):
+    data = dataframe.iloc[1:, 1:].astype(np.float).T
+    data = dataframe.dropna()
+    y = model.predict(data)
+    y = [0 if val < 0.5 else 1 for val in y]
+    for y = 1:
+        prediction="You will survive."
+    for y = 0:
+        prediction="You will not survive."
+    return prediction
 
