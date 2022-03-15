ecee300a301e9425275ebadefcb410e7a7893a07
ahutchful <74930349+ahutchful@users.noreply.github.com>
2022-03-14 21:09:06 -0400

    Update api.py

--- api.py
+++ api.py
@@ -194,8 +194,7 @@ class Userdata(BaseModel):
 
 @app.post("/prediction/")
 async def create_item(userdata: Userdata):
-    data = userdata.json()
-    df = pd.DataFrame(jsonable_encoder(data))
+    df = pd.DataFrame(jsonable_encoder(userdata))
     y = model.predict(df)
     y = [0 if val < 0.5 else 1 for val in y]
     if y == 1:
